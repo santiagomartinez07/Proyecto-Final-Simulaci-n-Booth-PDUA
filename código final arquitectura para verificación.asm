f79c769e8e4856eda3a27a094cd74b9cfce0b6c2
santiagomartinez07 <141145926+santiagomartinez07@users.noreply.github.com>
2023-11-18 20:09:45 -0500

    Update código final arquitectura para verificación.asm

--- código final arquitectura para verificación.asm
+++ código final arquitectura para verificación.asm
@@ -1,46 +1,47 @@
-variableA:0x0 
-Q:0b10000001 ; Multiplicador
-Q_1:0x0
-M: 0b11111101; Multiplicando
-count: 0x8
+; PROYECTO FINAL ALGORITMO BOOTH
+; Variables globales
 
+variableA: 0x0  ; Variable acumuladora para almacenar valores intermedios
+Q: 0b10000001   ; Multiplicando (el número que se está multiplicando)
+Q_1: 0x0        ; Variable temporal para almacenar el bit más significativo de Q
+M: 0b11111101  ; Multiplicador (el número por el que estamos multiplicando)
+count: 0x8      ; Variable contadora para llevar la cuenta del número de iteraciones
+
+; Inicio del programa
 inicio:
 
+; Comprueba si el bit más significativo de Q es 1
 mov ACC, Q
 mov DPTR, ACC
 mov ACC, [DPTR]
 mov A, ACC
 mov ACC, 0x1
 AND ACC, A
-mov A, ACC
+jz desplazamiento
 
+; Si el bit más significativo de Q es 0, salta a la rutina de suma
 mov ACC, Q_1
 mov DPTR, ACC
 mov ACC, [DPTR]
 add ACC, A
-mov A, ACC
-jz  desplazamiento
-
-mov ACC, Q
-mov DPTR, ACC
-mov ACC, [DPTR]
-mov A, ACC
-mov ACC, 0x1
-and ACC, A
-mov A, ACC
 jz suma
+
+; Si el bit más significativo de Q no es 0 ni 1, el programa termina
 jmp resta
 
+; Rutina de desplazamiento
 desplazamiento:
 
+; Comprueba si el bit más significativo de Q es 1
 mov ACC, Q
 mov DPTR, ACC
 mov ACC, [DPTR]
 mov A, ACC
 mov ACC, 0x1
-and ACC, A
-mov A, ACC
+AND ACC, A
+jz desplazamiento
 
+; Actualiza Q_1 y desplaza Q y variableA a la derecha
 mov ACC, Q_1
 mov DPTR, ACC
 mov ACC, A
@@ -48,8 +49,7 @@ mov [DPTR], ACC
 
 mov ACC, variableA
 mov DPTR, ACC
-mov ACC, [DPTR]
-rsh ACC, 0X1
+RSH ACC, 0x1
 mov A, ACC
 mov ACC, variableA
 mov DPTR, ACC
@@ -58,17 +58,19 @@ mov [DPTR], ACC
 
 mov ACC, Q
 mov DPTR, ACC
-mov ACC, [DPTR]
-rsh ACC, 0X1
+RSH ACC, 0x1
 mov A, ACC
 mov ACC, Q
 mov DPTR, ACC
 mov ACC, A
 mov [DPTR], ACC
+
 hlt
- 
+
+; Suma rutina
 suma:
 
+; Suma el producto de M y A al valor almacenado en variableA
 mov ACC, M
 mov DPTR, ACC
 mov ACC, [DPTR]
@@ -82,10 +84,14 @@ mov ACC, variableA
 mov DPTR, ACC
 mov ACC, A
 mov [DPTR], ACC
+
+; Salta a la rutina de desplazamiento
 jmp desplazamiento
 
+; Resta rutina
 resta:
 
+; Resta el producto de M y A al valor almacenado en variableA
 mov ACC, M
 mov DPTR, ACC
 mov ACC, [DPTR]
@@ -103,4 +109,6 @@ mov ACC, variableA
 mov DPTR, ACC
 mov ACC, A
 mov [DPTR], ACC
+
+; Salta a la rutina de desplazamiento
 jmp desplazamiento
